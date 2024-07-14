b0501fb916c399f00130a56ddbcbd6cbcbf41870
nadiaemg <nademarg@gmail.com>
2024-07-13 20:33:41 -0600

    Se concluyen las funciones de copiar y borrar. Se corrigen estilos y se agrega responsividad.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const textoIngresado = document.getElementById('textoIngresado');
 const textoEncriptado = document.getElementById('textoEncriptado');
+const contenedorVacio = document.querySelector('.contendor__encriptado__vacio');
+const contenedorLleno = document.querySelector('.contendor__encriptado__lleno');
 
 const reglasDeEncriptacion = {
     "a": "ai",
@@ -17,42 +19,75 @@ const reglasDeDesencriptacion = {
     "ufat": "u"
 };
 
-function encriptarTexto(cadenaDesencriptada, reglaDeEncriptacion) {
-    let arregloEncriptado = cadenaDesencriptada.split("").map(
+function encriptarTexto(cadenaParaEncriptar, reglaDeEncriptacion) {
+    cadenaParaEncriptar = cadenaParaEncriptar.split('').map(
         letra => reglaDeEncriptacion[letra] || letra
     );
-    return arregloEncriptado.join("");
+    return cadenaParaEncriptar.join('');
 }
 
-function desencriptarTexto(cadenaEncriptada, reglaDeDesencriptacion) {
-    let cadenaDesencriptada = cadenaEncriptada;
+function desencriptarTexto(cadenaParaDesencriptar, reglaDeDesencriptacion) {
     for (let letraEncriptada in reglaDeDesencriptacion) {
-        cadenaDesencriptada = cadenaDesencriptada.split(letraEncriptada).join(reglaDeDesencriptacion[letraEncriptada]);
+        cadenaParaDesencriptar = cadenaParaDesencriptar.split(letraEncriptada).join(reglaDeDesencriptacion[letraEncriptada]);
     }
-    return cadenaDesencriptada;
+    return cadenaParaDesencriptar;
 }
 
-function mostrarEncriptacion() {
-    textoEncriptado.innerText = encriptarTexto(textoIngresado.value, reglasDeEncriptacion);
-    mostrarOcultarTexto();
-}
+//Función para validar y mostrar el texto encriptado/desencriptado
+function validarYMostrarTexto(opcion) {
+    const regex = /^[a-z\s]+$/;
+    const textoIngresadoValue = textoIngresado.value.trim();
 
-function mostrarDesencriptacion() {
-    textoEncriptado.innerText = desencriptarTexto(textoIngresado.value, reglasDeDesencriptacion);
-    mostrarOcultarTexto();
-}
-
-function mostrarOcultarTexto() {
-    const contenedorVacio = document.querySelector('.contendor__encriptado__vacio');
-    const contenedorLleno = document.querySelector('.contendor__encriptado__lleno');
-
-    if(textoIngresado.value.length > 0) {
-        contenedorLleno.style.display = "flex";
-        contenedorVacio.style.display = "none";
+    if (textoIngresadoValue.length === 0) {
+        contenedorVacio.style.display = 'flex';
+        contenedorLleno.style.display = 'none';
+    } else if (regex.test(textoIngresadoValue)) {
+        contenedorLleno.style.display = 'flex';
+        contenedorVacio.style.display = 'none';
+        if (opcion === 'encriptar') {
+            textoEncriptado.innerText = encriptarTexto(textoIngresadoValue, reglasDeEncriptacion);
+        } else if (opcion === 'desencriptar') {
+            textoEncriptado.innerText = desencriptarTexto(textoIngresadoValue, reglasDeDesencriptacion);
+        }
     } else {
-        contenedorVacio.style.display = "flex";
-        contenedorLleno.style.display = "none";
+        Swal.fire({
+            icon: "info",
+            text: " Solo se permiten letras minúsculas. No se aceptan caracteres especiales, números o letras mayúsculas.",
+        });
     }
+}
+
+function copiarTexto() {
+    //Seleccionar texto
+    const rango = document.createRange();
+    const seleccion = window.getSelection();
+
+    rango.selectNodeContents(textoEncriptado);
+    seleccion.removeAllRanges();
+    seleccion.addRange(rango);
+
+    //Guardar texto en portapapeles
+    navigator.clipboard.writeText(textoEncriptado.innerText).then(function () {
+        console.log('Texto copiado al portapapeles');
+        const tooltip = document.getElementById('tooltipTexto');
+
+        // Mostrar tooltip por 15 segundos
+        tooltip.style.display = 'block'
+
+        setTimeout(function () {
+            tooltip.style.display = 'none'
+        }, 5000);
+
+    }).catch(function (err) {
+        console.error('Error al copiar el texto: ', err);
+    });
+}
+
+function borrarTexto() {
+    textoEncriptado.innerText = "";
+    textoIngresado.value = "";
+    contenedorVacio.style.display = 'flex';
+    contenedorLleno.style.display = 'none';
 }
 
 //Función para poner año en footer
@@ -62,6 +97,6 @@ function year() {
     yearSpan.innerText = currentYear;
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     year()
 });
